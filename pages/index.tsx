13887c5e070a77df15720e89e81202c1d5f5c194
Italo Gustavo <italogustavo.dev@gmail.com>
2022-07-31 23:58:48 -0300

    add next link

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
         <Link href="/about">about link</Link>
-        <a href="/api/auth/login">Login</a>
+        <Link href="/api/auth/login">Login</Link>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
